c2f5a2d84f4586480b41d9b4713bfd3312f4c86c
ojohnson1 <ojohnson659@gmail.com>
2023-10-26 10:58:16 -0400

    finished date exercise

--- Dates/scripts/date_to_string.js
+++ Dates/scripts/date_to_string.js
@@ -6,5 +6,4 @@ let d = new Date();
 console.log (d.toDateString());
 console.log (d.toString());
 console.log (d.toUTCString());
-
-console.log(d.toLocaleString());
+console.log (d.toLocaleString());
--- Dates/scripts/more_date_conversions.js
+++ Dates/scripts/more_date_conversions.js
@@ -1,7 +1,11 @@
 "use strict"
 
-let birthDate = new Date(2/23/1997)
-
+let birthDate =new Date ( "02/23/1997")
+let momBirthDate= new Date(" Aug 29 1974")
+let dadBirthDate= new Date ("1974, 7, 23")
 
 
 console.log(birthDate.toLocaleString());
+console.log(momBirthDate.toLocaleString());
+console.log(dadBirthDate.toLocaleString());
+
