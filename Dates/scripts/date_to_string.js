4640f375e0d2f9fe1b1eb261f50febd110689fdb
ojohnson1 <ojohnson659@gmail.com>
2023-10-26 09:24:03 -0400

    date exercises

---
+++ Dates/scripts/date_to_string.js
@@ -0,0 +1,10 @@
+"use strict"
+
+
+let d = new Date();
+
+console.log (d.toDateString());
+console.log (d.toString());
+console.log (d.toUTCString());
+
+console.log(d.toLocaleString());
---
+++ Dates/scripts/more_date_conversions.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let birthDate = new Date(2/23/1997)
+
+
+
+console.log(birthDate.toLocaleString());
